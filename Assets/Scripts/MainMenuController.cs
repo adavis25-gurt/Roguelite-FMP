1380e613d52a0dee6bb3283673adda761b45e92d
adavis25-gurt <adavis25@elam.co.uk>
2026-03-10 15:39:59 +0000

    main menu done

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,56 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] VisualElement ui;
+
+    [SerializeField] Button StoryButton;
+    [SerializeField] Button EndlessButton;
+    [SerializeField] Button SettingsButton;
+    [SerializeField] Button ExitButton;
+
+    private void Awake()
+    {
+        ui = GetComponent<UIDocument>().rootVisualElement;
+    }
+
+    private void OnEnable()
+    {
+        StoryButton = ui.Q<Button>("StoryButton");
+        StoryButton.clicked += OnStoryButtonClicked;
+
+        EndlessButton = ui.Q<Button>("EndlessButton");
+        EndlessButton.clicked += OnEndlessButtonClicked;
+
+        SettingsButton = ui.Q<Button>("SettingsButton");
+        SettingsButton.clicked += OnSettingsButtonClicked;
+
+        ExitButton = ui.Q<Button>("ExitButton");
+        ExitButton.clicked += OnExitButtonClicked;
+    }
+
+    void OnStoryButtonClicked()
+    {
+        print("ye go to story mode chud");
+    }
+
+    void OnEndlessButtonClicked()
+    {
+        print("ye go to endless mode chud");
+    }
+
+    void OnSettingsButtonClicked()
+    {
+        print("ye go to settings chud");
+    }
+
+    void OnExitButtonClicked()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#endif
+    }
+}
